e40da754c6f84824e25003321e1f93fb33a377c9
Lawrence Finn <laf2019@columbia.edu>
2018-03-08 21:08:54 -0500

    compact mode

--- signer/signer.go
+++ signer/signer.go
@@ -10,10 +10,10 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) < 3 || len(os.Args) > 4 {
 		fmt.Println("Signs the provided data (hex encoded) with the provided private key (hex encoded) " +
 			"using the Bitcoin ECDSA curve.")
-		fmt.Println("Usage: signer [datahex] [privatehex]")
+		fmt.Println("Usage: signer [datahex] [privatehex] [optional c for compressed]")
 		return
 	}
 	data, err := hex.DecodeString(os.Args[1])
@@ -25,11 +25,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sig, err := Sign(priv, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(hex.EncodeToString(sig))
+    if len(os.Args) == 3 || os.Args[3] != "c" {
+        sig, err := Sign(priv, data)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(hex.EncodeToString(sig))
+    } else {
+        compact, err := SignC(priv, data)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(hex.EncodeToString(compact))
+    }
+       
+
 }
 
 func Sign(private, data []byte) ([]byte, error) {
@@ -39,4 +49,13 @@ func Sign(private, data []byte) ([]byte, error) {
 		return nil, err
 	}
 	return sig.Serialize(), nil
+}
+
+func SignC(private, data []byte) ([]byte, error) {
+	privkey, _ := btcec.PrivKeyFromBytes(btcec.S256(), private)
+	sig, err := btcec.SignCompact(btcec.S256(), privkey, data, false)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
 }
